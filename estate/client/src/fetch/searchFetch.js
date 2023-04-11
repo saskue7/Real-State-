c5d15586c678337cfa0b7831ed21247374cbf4d8
saskue7 <55278429+saskue7@users.noreply.github.com>
2023-04-11 23:55:17 +0530

    Update searchFetch.js

--- estate/client/src/fetch/searchFetch.js
+++ estate/client/src/fetch/searchFetch.js
@@ -5,7 +5,7 @@ export const baseUrl = 'https://bayut.p.rapidapi.com';
 export const fetchApi = async (url) => {
  const { data } = await axios.get((url), {
   headers: {
-   'X-RapidAPI-Key': '022111aa49mshb3ca0ce5bb3c604p15cdc3jsnd41ea281305b',
+   'X-RapidAPI-Key': 'type your api',
    'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
   }
  });
